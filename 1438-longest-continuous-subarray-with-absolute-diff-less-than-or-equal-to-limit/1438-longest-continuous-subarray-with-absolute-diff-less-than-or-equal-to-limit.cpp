3b1094e87a470aca36f8a56ef546b6886ff07e43
mann220 <saraswatmanvendra@gmail.com>
2026-07-31 18:18:38 +0530

    Time: 16 ms (84.15%), Space: 77.6 MB (62.88%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
+    // this is done in O(nlogn) for O(n) we have to do it with deque
     int longestSubarray(vector<int>& nums, int limit) {
         int n=nums.size();
-        multiset<int> st;
+        deque<int> maxh,minh;
         int i=0;
         int ln=0;
         for(int j=0;j<n;j++){
-            st.insert(nums[j]);
-            while(*prev(st.end())-*st.begin()>limit){
-                st.erase(st.find(nums[i]));
+            while(!maxh.empty() && nums[maxh.back()]<nums[j]) maxh.pop_back();
+            maxh.push_back(j);
+            while(!minh.empty() && nums[minh.back()]>nums[j]) minh.pop_back();
+            minh.push_back(j);
+            while(nums[maxh.front()]-nums[minh.front()]>limit){
+                if(maxh.front()==i) maxh.pop_front();
+                if(minh.front()==i) minh.pop_front();
                 i++;
             }
             ln=max(ln,j-i+1);
