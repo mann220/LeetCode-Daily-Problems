4e0de2a18eb469dd854dc03e8a0c51bcd5f81f28
mann220 <saraswatmanvendra@gmail.com>
2026-08-01 11:55:37 +0530

    Time: 147 ms (8.11%), Space: 9.6 MB (85.12%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int f(int i,int j,vector<int> &nums){
         if(i>j) return 0;
-        int take1=nums[i]+min(f(i+2,j,nums),f(i+1,j-1,nums));
-        int take2=nums[j]+min(f(i,j-2,nums),f(i+1,j-1,nums));
+        int take1=nums[i]-f(i+1,j,nums);
+        int take2=nums[j]-f(i,j-1,nums);
         return max(take1,take2);
     }
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
         int sum=accumulate(nums.begin(),nums.end(),0);
-        int plyr1=f(0,n-1,nums);
-        int plyr2=sum-plyr1;
-        return plyr1>=plyr2;
+        return f(0,n-1,nums)>=0;
     }
 };
