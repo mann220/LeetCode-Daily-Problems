1c36817b31642ac8db54ec081cabe66c06dafe75
mann220 <saraswatmanvendra@gmail.com>
2026-08-02 13:01:11 +0530

    Time: 79 ms (20%), Space: 267.7 MB (20%) - LeetHub

--- 4008-minimum-initial-strength-to-defeat-all-monsters/4008-minimum-initial-strength-to-defeat-all-monsters.cpp
+++ 4008-minimum-initial-strength-to-defeat-all-monsters/4008-minimum-initial-strength-to-defeat-all-monsters.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     #define ll long long
-    bool f(ll mid,vector<int>& m,vector<ll> &b){
-        int n=m.size();
-        for(int i=0;i<n;i++){
-            if(mid+b[i]<m[i]) return false;
-            mid-=m[i];
-            if(mid<0) mid=0;
-        }
-        return true;
-    }
     long long minInitialStrength(vector<int>& monsters, vector<vector<int>>& boosts) {
         int n=monsters.size();
         vector<ll> dif(n+1,0);
@@ -27,17 +18,11 @@ public:
             sum+=dif[i];
             bonus[i]=sum;
         }
-        ll lo=0;
-        ll hi=accumulate(monsters.begin(),monsters.end(),0LL);
-        ll ans;
-        while(lo<=hi){
-            ll mid=(lo+hi)/2;
-            if(f(mid,monsters,bonus)){
-                ans=mid;
-                hi=mid-1;
-            }
-            else lo=mid+1;
-        }        
-        return ans;
+        ll req=0;
+        for(int i=n-1;i>=0;i--){
+            if(req>0) req+=monsters[i];
+            else req=max(0LL,(ll)monsters[i]-bonus[i]);
+        } 
+        return req;
     }
 };
