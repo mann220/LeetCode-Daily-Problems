27eb021dbd2c686d113ead3460b0e25ea2b1e8df
mann220 <saraswatmanvendra@gmail.com>
2026-08-04 12:16:17 +0530

    Time: 248 ms (26.78%), Space: 19.2 MB (65.66%) - LeetHub

---
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long wonderfulSubstrings(string s) {
+        int n=s.size();
+        int mask=0;
+        unordered_map<int,int> mpp;
+        mpp[0]=1;
+        long long ans=0;
+        for(int i=0;i<n;i++){
+            mask^=(1<<(s[i]-'a'));
+            ans+=mpp[mask];
+            for(char ch='a';ch<='j';ch++){
+                int xr=(mask ^ (1<<(ch-'a')));
+                if(mpp.find(xr)!=mpp.end()){
+                    ans+=mpp[xr];
+                }
+            }
+            mpp[mask]++;
+        }
+        return ans;
+    }
+};
