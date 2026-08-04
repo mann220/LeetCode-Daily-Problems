9caaacc51991dc7b89544c437a360197dbc7debb
mann220 <saraswatmanvendra@gmail.com>
2026-08-04 12:47:57 +0530

    Time: 79 ms (15.4%), Space: 22.2 MB (10.95%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,19 +2,26 @@ class Solution {
 public:
     // my observations says its dp in which we have to try all possibilities 
     int n;
-    vector<int> dp;
-    long long f(int i,vector<int> &nums){
-        if(i>=n-1) return 0;
-        if(dp[i]!=-1) return dp[i];
-        long long ans=INT_MAX;
-        for(int j=i+1;j<=i+nums[i];j++){
-            ans=min(ans,1+f(j,nums));
-        }
-        return dp[i]=ans;
-    }
+    // vector<long long> dp;
+    // long long f(int i,vector<int> &nums){
+    //     if(i>=n-1) return 0;
+    //     if(dp[i]!=-1) return dp[i];
+    //     long long ans=INT_MAX;
+    //     for(int j=i+1;j<=i+nums[i];j++){
+    //         ans=min(ans,1+f(j,nums));
+    //     }
+    //     return dp[i]=ans;
+    // }
     int jump(vector<int>& nums) {
         n=nums.size();
-        dp.assign(n,-1);
-        return (int)f(0,nums);
+        vector<long long> dp(n,INT_MAX);
+        dp[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            long long ans=INT_MAX;
+            for(int j=i+1;j<=min(n-1,i+nums[i]);j++){
+                dp[i]=min(dp[i],1+dp[j]);
+            }
+        }
+        return (int)dp[0];
     }
 };
