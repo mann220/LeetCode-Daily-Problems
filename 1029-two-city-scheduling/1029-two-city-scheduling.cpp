a9e802386cb80a48fb40906bbe1d62e1112f0e17
mann220 <saraswatmanvendra@gmail.com>
2026-08-08 00:02:23 +0530

    Time: 0 ms (100%), Space: 10.9 MB (82.69%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int n;
-    vector<vector<vector<int>>> dp;
-    int f(int i,vector<vector<int>>& costs,int cnta,int cntb){
-        if(i==n) return 0;
-        if(dp[i][cnta][cntb]!=-1) return dp[i][cnta][cntb];
-        int takea=INT_MAX,takeb=INT_MAX;
-        if(cnta>0) takea=costs[i][0]+f(i+1,costs,cnta-1,cntb);
-        if(cntb>0) takeb=costs[i][1]+f(i+1,costs,cnta,cntb-1);
-        return dp[i][cnta][cntb]=min(takea,takeb);
-    }
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        n=costs.size();
-        dp.assign(n,vector<vector<int>>((n/2)+1,vector<int>((n/2)+1,-1)));
-        return f(0,costs,n/2,n/2);
+        int n=costs.size();
+        priority_queue<pair<int,int>> pq;
+        for(int i=0;i<costs.size();i++){
+            pq.push({costs[i][0]-costs[i][1],i});
+        }
+        int sum=0;
+        int half=n/2;
+        while(half--){
+            int i=pq.top().second;
+            pq.pop();
+            sum+=(costs[i][1]);
+        }
+        while(!pq.empty()){
+            int i=pq.top().second;
+            pq.pop();
+            sum+=(costs[i][0]);
+        }
+        return sum;
     }
 };
