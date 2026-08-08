7cdbc576d9635a27893fa5d8d97dd172e4280d60
mann220 <saraswatmanvendra@gmail.com>
2026-08-08 10:39:40 +0530

    Time: 309 ms (5.02%), Space: 159.8 MB (5.03%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> ans;
-        for(int mask=0;mask<(1<<n);mask++){
-            if(__builtin_popcount(mask)!=k) continue;
-            vector<int> temp;
-            for(int i=0;i<n;i++){
-                if(mask & (1<<i)){
-                    temp.push_back(i+1);
-                }
-            }
+    vector<vector<int>> ans;
+    void f(int i,int n,int k,vector<int> temp){
+        if(temp.size()==k){
             ans.push_back(temp);
+            return;
         }
+        for(int j=i;j<=n;j++){
+            temp.push_back(j);
+            f(j+1,n,k,temp);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> temp;
+        f(1,n,k,temp);
         return ans;
     }
 };
