2ab13b70130fa441f137d078ad878d137d59dfdb
mann220 <saraswatmanvendra@gmail.com>
2026-08-09 11:32:36 +0530

    Time: 36 ms (26.83%), Space: 16.5 MB (79.23%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -4,12 +4,14 @@ public:
         int n=nums.size();
         sort(nums.begin(),nums.end());
         int cnt=0;
-        for(int i=0;i<n-2;i++){
-            for(int j=i+1;j<n-1;j++){
-                int val=nums[i]+nums[j];
-                int lb=lower_bound(nums.begin()+j+1,nums.end(),val)-nums.begin();
-                // cout<<val<<" "<<lb<<endl;
-                cnt+=(lb-1-j);
+        for(int i=2;i<n;i++){
+            int l=0,r=i-1;
+            while(l<r){
+                if(nums[l]+nums[r]>nums[i]){
+                    cnt+=(r-l);
+                    r--;
+                }
+                else l++;
             }
         }
         return cnt;
