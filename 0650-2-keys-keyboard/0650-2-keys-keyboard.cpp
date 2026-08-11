71cad5d00f52b039d00d19d73448c1f9c3795138
mann220 <saraswatmanvendra@gmail.com>
2026-08-11 23:04:54 +0530

    Time: 73 ms (6.98%), Space: 157.2 MB (20.28%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int n;
+    vector<vector<int>> dp;
     int f(int curr,int clip){
         if(curr==n) return 0;
         if(curr>n) return 1e9;
+        if(dp[curr][clip]!=-1) return dp[curr][clip];
         // option 1 is copy all
         int copy=1e9;
         if(curr!=clip) copy=1+f(curr,curr);
@@ -12,11 +14,11 @@ public:
         if(clip>0){
             paste=1+f(curr+clip,clip);
         }
-        return min(copy,paste);
+        return dp[curr][clip]=min(copy,paste);
     }
-    int minSteps(int p) {
-        n=p;
-        if(n==1) return 0;
+    int minSteps(int n) {
+        this->n=n;
+        dp.assign(n,vector<int> (n,-1));
         return f(1,0);
     }
 };
