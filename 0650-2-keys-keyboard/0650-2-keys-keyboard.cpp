3aefb716f430b8da9b820e58bcecfdeaac643aa2
mann220 <saraswatmanvendra@gmail.com>
2026-08-11 23:24:55 +0530

    Time: 0 ms (100%), Space: 7.8 MB (96.06%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
-    int n;
-    vector<vector<int>> dp;
-    // int f(int curr,int clip){
-    //     if(curr==n) return 0;
-    //     if(curr>n) return 1e9;
-    //     if(dp[curr][clip]!=-1) return dp[curr][clip];
-    //     // option 1 is copy all
-    //     int copy=1e9;
-    //     if(curr!=clip) copy=1+f(curr,curr);
-    //     // option 2 is paste them
-    //     int paste=1e9;
-    //     if(clip>0){
-    //         paste=1+f(curr+clip,clip);
-    //     }
-    //     return dp[curr][clip]=min(copy,paste);
-    // }
     int minSteps(int n) {
-        this->n=n;
-        dp.assign(n+1,vector<int> (n+1,0));
-        for(int i=n-1;i>=1;i--){
-            for(int j=n-1;j>=0;j--){
-                int copy=1e9,paste=1e9;
-                if(i!=j) copy=1+dp[i][i];
-                if(j>0 && i+j<=n) paste=1+dp[i+j][j];
-                dp[i][j]=min(copy,paste);
+        // we can do it by prime factorisation
+        int ans=0;
+        for(int i=2;i*i<=n;i++){
+            while(n%i==0){
+                ans+=i;
+                n/=i;
             }
         }
-        return dp[1][0];
+        if(n>1){
+            ans+=n;
+        }
+        return ans;
     }
 };
