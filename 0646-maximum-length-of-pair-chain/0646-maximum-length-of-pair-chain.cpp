5220f2b8cea3424965c1124ff0d5b491162b07ab
mann220 <saraswatmanvendra@gmail.com>
2026-08-11 16:52:40 +0530

    Time: 765 ms (5.06%), Space: 391.9 MB (8.78%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -3,18 +3,26 @@ public:
     int n;
     int off=1001;
     vector<vector<int>> dp;
-    int f(int i,int last,vector<vector<int>>& pairs){
-        if(i==n) return 0;
-        if(dp[i][last+off]!=-1) return dp[i][last+off];
-        int take=INT_MIN;
-        if(pairs[i][0]>last) take=1+f(i+1,pairs[i][1],pairs);
-        int nottake=f(i+1,last,pairs);
-        return dp[i][last+off]=max(take,nottake);
-    }
+    // int f(int i,int last,vector<vector<int>>& pairs){
+    //     if(i==n) return 0;
+    //     if(dp[i][last+off]!=-1) return dp[i][last+off];
+    //     int take=INT_MIN;
+    //     if(pairs[i][0]>last) take=1+f(i+1,pairs[i][1],pairs);
+    //     int nottake=f(i+1,last,pairs);
+    //     return dp[i][last+off]=max(take,nottake);
+    // }
     int findLongestChain(vector<vector<int>>& pairs) {
         n=pairs.size();
         sort(pairs.begin(),pairs.end());
-        dp.assign(n+1,vector<int> (2002,-1));
-        return f(0,-1001,pairs);
+        dp.assign(n+1,vector<int> (2002,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=-1001;j<=1000;j++){
+                int take=INT_MIN;
+                if(pairs[i][0]>j) take=1+dp[i+1][pairs[i][1]+off];
+                int nottake=dp[i+1][j+off];
+                dp[i][j+off]=max(take,nottake);
+            }
+        }
+        return dp[0][0];
     }
 };
