0724cd64f4291bfeb0a109cb944f34180fae7d8b
mann220 <saraswatmanvendra@gmail.com>
2026-08-11 19:23:57 +0530

    Time: 3 ms (85.64%), Space: 8.5 MB (91.54%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    vector<vector<bool>> dp;
-    // bool check(int i,int j,string &s){
-    //     if(i>j) return true;
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     if(s[i]==s[j]){
-    //         return dp[i][j]=check(i+1,j-1,s);
-    //     }
-    //     return dp[i][j]=false;
-    // }
+    int ans;
+    int n;
+    void check(int i,int j,string &s){
+        while(i>=0 && j<n && s[i]==s[j]){
+            ans++;
+            i--;
+            j++;
+        }
+    }
     int countSubstrings(string s) {
-        int n=s.size();
-        dp.assign(n+1,vector<bool>(n+1,false));
-        int ans=0;
-        // Blue print for this type of palindromic question
-        for(int l=1;l<=n;l++){
-            for(int i=0;i+l-1<n;i++){
-                int j=i+l-1;
-                if(i==j) dp[i][i]=true;
-                else if(i+1==j) dp[i][j]=(s[i]==s[j]);
-                else{
-                    dp[i][j]=(s[i]==s[j] && dp[i+1][j-1]);
-                }
-                if(dp[i][j]) ans++;
-            }
+        n=s.size();
+        ans=0;
+        for(int i=0;i<n;i++){
+            check(i,i,s);
+            check(i,i+1,s);
         }
         return ans;
     }
