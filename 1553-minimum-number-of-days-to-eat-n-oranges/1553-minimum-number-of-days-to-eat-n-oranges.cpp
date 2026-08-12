e7c9ea7d2e578f426c781bc610a579b7ecb01ee5
mann220 <saraswatmanvendra@gmail.com>
2026-08-12 18:49:32 +0530

    Time: 6 ms (77.36%), Space: 11.7 MB (37.11%) - LeetHub

--- 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.cpp
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
+    unordered_map<int,int> mpp;
     int f(int n){
         if(n==0 || n==1) return n;
+        if(mpp.count(n)) return mpp[n]; 
         // we have two options
-        return 1+min(n%2+f(n/2),n%3+f(n/3));
+        return mpp[n]=1+min(n%2+f(n/2),n%3+f(n/3));
     }
     int minDays(int n) {
         return f(n);
