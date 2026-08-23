5a1a86c238aecd52c0975ebdf90f4a2372a262ca
mann220 <saraswatmanvendra@gmail.com>
2026-08-23 22:26:30 +0530

    Time: 4 ms (47.55%), Space: 13.9 MB (67.83%) - LeetHub

--- 1927-sum-game/1927-sum-game.cpp
+++ 1927-sum-game/1927-sum-game.cpp
@@ -1,49 +1,18 @@
 class Solution {
 public:
-    // acc to my observation there are two conditon possible if alice want to maximise the first half and minimise the second half or it maximise the second half and minise the frist half
-    bool sumGame(string s) {
-        int n=s.size();
-        int f1sum=0,f2sum=0;
-        int l1sum=0,l2sum=0;
-        bool flag=true;
+    bool sumGame(string num) {
+        int n=num.size();
+        double ans=0;
         for(int i=0;i<n;i++){
-            if(flag){
-                if(i<n/2 && s[i]=='?'){
-                    f1sum+=0;
-                    f2sum+=9;
-                    flag=false;
-                    continue;
-                }
-                else if(i>=n/2 && s[i]=='?'){
-                    l1sum+=9;
-                    l2sum+=0;
-                    flag=false;
-                    continue;
-                }
-            }
-            else{
-                if(i<n/2 && s[i]=='?'){
-                    f1sum+=9;
-                    f2sum+=0;
-                    flag=true;
-                    continue;
-                }
-                if(i>=n/2 && s[i]=='?'){
-                    l1sum+=0;
-                    l2sum+=9;
-                    flag=true;
-                    continue;
-                }
-            }
             if(i<n/2){
-                f1sum+=(s[i]-'0');
-                f2sum+=(s[i]-'0');
-            }
+                if(num[i]=='?') ans+=4.5;
+                else ans+=num[i]-'0';
+            } 
             else{
-                l1sum+=(s[i]-'0');
-                l2sum+=(s[i]-'0');
+                if(num[i]=='?') ans-=4.5;
+                else ans-=num[i]-'0';
             }
         }
-        return (f1sum<l1sum || f2sum>l2sum);
+        return ans!=0.0;
     }
 };
