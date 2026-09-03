627ea8fd7c2e68fd9028acad767ec8cc373de906
mann220 <saraswatmanvendra@gmail.com>
2026-09-03 18:18:06 +0530

    Time: 4 ms (69.78%), Space: 11.5 MB (65.07%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -3,9 +3,7 @@ public:
     string minWindow(string s, string t) {
         int n=s.size();
         int m=t.size();
-        int minLen=1e9;
-        int sIndex=-1;
-        int cnt=0;
+        int cnt=0,sInd=-1,minLen=1e9;
         unordered_map<char,int> mpp;
         for(int i=0;i<m;i++) mpp[t[i]]++;
         int i=0,j=0;
@@ -15,14 +13,14 @@ public:
             while(cnt==m){
                 if(j-i+1<minLen){
                     minLen=j-i+1;
-                    sIndex=i; 
+                    sInd=i;
                 }
                 mpp[s[i]]++;
-                if(mpp[s[i]]>0) cnt=cnt-1;
+                if(mpp[s[i]]>0) cnt--;
                 i++;
             }
             j++;
         }
-        return sIndex==-1 ? "":s.substr(sIndex,minLen);
+        return sInd==-1 ? "": s.substr(sInd,minLen); 
     }
 };
