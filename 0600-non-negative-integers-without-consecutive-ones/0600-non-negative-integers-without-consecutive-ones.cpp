c722098981ab92eef1bade10a8fd33e79a6f8650
mann220 <saraswatmanvendra@gmail.com>
2026-09-04 12:50:32 +0530

    Time: 0 ms (100%), Space: 8.2 MB (85.04%) - LeetHub

--- 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
@@ -6,12 +6,15 @@ public:
         dp[0]=1;
         dp[1]=2;
         for(int i=2;i<31;i++) dp[i]=dp[i-1]+dp[i-2];
+        int prev=0;
         int ans=0;
         for(int i=30;i>=0;i--){
             if(n & (1<<i)){
                 ans+=dp[i];
-                if(i<30 && (n & (1<<(i+1)))) return ans;
+                if(prev==1) return ans;
+                prev=1;
             }
+            else prev=0;
         }
         return ans+1;
     }
