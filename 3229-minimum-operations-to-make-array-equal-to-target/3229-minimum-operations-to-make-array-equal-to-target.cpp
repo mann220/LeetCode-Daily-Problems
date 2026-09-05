fd04dfd8aff0cbc568fa5adab4cac130453b50eb
mann220 <saraswatmanvendra@gmail.com>
2026-09-05 14:17:03 +0530

    Time: 0 ms (100%), Space: 182.2 MB (58.62%) - LeetHub

--- 3229-minimum-operations-to-make-array-equal-to-target/3229-minimum-operations-to-make-array-equal-to-target.cpp
+++ 3229-minimum-operations-to-make-array-equal-to-target/3229-minimum-operations-to-make-array-equal-to-target.cpp
@@ -1,44 +1,17 @@
 class Solution {
 public:
     #define ll long long
-    ll f(vector<ll> temp){
-        ll prev=abs(temp[0]);
-        ll ans=abs(temp[0]);
-        ll curr=0;
-        for(int i=1;i<temp.size();i++){
-            curr=abs(temp[i]);
-            if(curr>prev) ans+=(curr-prev);
-            prev=curr;
-        }
-        return ans;
-    }
     long long minimumOperations(vector<int>& nums, vector<int>& target) {
         int n=nums.size();
-        vector<ll> diff(n);
-        for(int i=0;i<n;i++) diff[i]=target[i]-nums[i];
         ll ans=0;
-        int i=0,j=0;
-        while(i<n){
-            vector<ll> temp;
-            if(diff[i]==0){
-                i++;
-                continue;
-            }
-            if(diff[i]>=0){
-                while(j<n && diff[j]>=0){
-                    temp.push_back(diff[j]);
-                    j++;
-                }
-                ans+=f(temp);
-            }
-            else{
-                while(j<n && diff[j]<=0){
-                    temp.push_back(diff[j]);
-                    j++;
-                }
-                ans+=f(temp);
-            }
-            i=j;
+        int curr=0;
+        int prev=0;
+        for(int i=0;i<n;i++){
+            curr=target[i]-nums[i];
+            // sign change
+            if((curr>0 && prev<0) || (curr<0 && prev>0)) ans+=abs(curr);
+            else if(abs(prev)<abs(curr)) ans+=(abs(curr)-abs(prev));
+            prev=curr;
         }
         return ans;
     }
