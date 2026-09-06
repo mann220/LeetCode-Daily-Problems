9171f78f1d7fe408d99bcb9732ec20ffc85338c2
mann220 <saraswatmanvendra@gmail.com>
2026-09-06 20:16:54 +0530

    Time: 688 ms (77.56%), Space: 263 MB (56.36%) - LeetHub

--- 1044-longest-duplicate-substring/1044-longest-duplicate-substring.cpp
+++ 1044-longest-duplicate-substring/1044-longest-duplicate-substring.cpp
@@ -19,31 +19,45 @@ public:
     // }
 
     // Rabin Karp
-    // best way to prevent leetcode antihash is unsigned long long
-    #define ull unsigned long long 
-
-    // ll mod=1e9+7;
-    ull radix=131;
-    ull hashValue(string &s,int len){
-        ull ans=0,factor=1;
+    #define ll long long
+    ll mod=1e9+7;
+    ll radix=26;
+    ll hashValue(string s,int len){
+        ll ans=0,factor=1;
         for(int i=len-1;i>=0;i--){
-            ans+=((s[i]-'a')*factor);
-            factor=(factor*radix);
+            ans+=((s[i]-'a')*factor)%mod;
+            factor=(factor*radix)%mod;
         }
-        return ans;
+        return ans%mod;
+    }
+    bool checkMatch(const string &s, int idx1, int idx2, int len) {
+        for (int k=0;k<len;k++) {
+            if(s[idx1+k]!=s[idx2+k]){
+                return false;
+            }
+        }
+        return true;
     }
     string f(int len,string &s){
         int n=s.size();
-        ull max_weight=1;
-        unordered_map<ull,int> mpp;
-        for(int i=0;i<len;i++) max_weight=max_weight*radix;
-        ull hash1=hashValue(s,len);
-        ull hash2=0;
+        ll max_weight=1;
+        unordered_map<ll,ll> mpp;
+        for(int i=0;i<len;i++) max_weight=(max_weight*radix)%mod;
+        ll hash1=hashValue(s,len);
+        ll hash2=0;
         for(int i=0;i<=s.size()-len;i++){
             if(i==0) hash2=hash1;
-            else hash2=((hash2*radix)-((s[i-1]-'a')*max_weight)+(s[i+len-1]-'a'));
-            mpp[hash2]++;
-            if(mpp[hash2]>=2) return s.substr(i,len);
+            else hash2=((hash2*radix)%mod-((s[i-1]-'a')*max_weight)%mod+(s[i+len-1]-'a')+mod)%mod;
+            if (mpp.find(hash2) != mpp.end()) {
+                // Check if the current string matches the string at the stored index
+                if (checkMatch(s, mpp[hash2], i, len)) {
+                    return s.substr(i, len);
+                }
+            } else {
+                // Only insert if it's a new hash. If it's a false collision, 
+                // we just keep the old index in the map.
+                mpp[hash2] = i;
+            }
         }
         return "";
     }
