32128bd024f3b8a99ea1983bed0d7f3bc94dff0e
mann220 <saraswatmanvendra@gmail.com>
2026-09-06 18:42:24 +0530

    Time: 512 ms (66.71%), Space: 135.8 MB (17.9%) - LeetHub

--- 4046-minimum-cost-path-with-at-most-k-turns/4046-minimum-cost-path-with-at-most-k-turns.cpp
+++ 4046-minimum-cost-path-with-at-most-k-turns/4046-minimum-cost-path-with-at-most-k-turns.cpp
@@ -1,46 +1,39 @@
 class Solution {
 public:
-    // dir=0 -> left , dir=1-> right, dir=2-> up, dir=3-> down
-    int dp[75][75][75][5];
-    int f(int i,int j,int k,int dir,vector<vector<int>>& grid){
-        int n=grid.size();
-        int m=grid[0].size();
-        if(i<0 || j<0 || i>=n || j>=m) return 1e9;
-        if(i==n-1 && j==m-1) return grid[i][j];
-        if(dp[i][j][k][dir]!=-1) return dp[i][j][k][dir];
-        int ans=1e9;
-        if(dir==4 || dir==0){
-            ans=min(ans,grid[i][j]+f(i,j-1,k,0,grid));
-            if(k>0) ans=min(ans,grid[i][j]+f(i,j+1,k-1,1,grid));
-            if(k>0) ans=min(ans,grid[i][j]+f(i-1,j,k-1,2,grid));
-            if(k>0) ans=min(ans,grid[i][j]+f(i+1,j,k-1,3,grid));
-        }
-        if(dir==4 || dir==1){
-            if(k>0) ans=min(ans,grid[i][j]+f(i,j-1,k-1,0,grid));
-            ans=min(ans,grid[i][j]+f(i,j+1,k,1,grid));
-            if(k>0) ans=min(ans,grid[i][j]+f(i-1,j,k-1,2,grid));
-            if(k>0) ans=min(ans,grid[i][j]+f(i+1,j,k-1,3,grid));
-        }
-        if(dir==4 || dir==2){
-            if(k>0) ans=min(ans,grid[i][j]+f(i,j-1,k-1,0,grid));
-            if(k>0) ans=min(ans,grid[i][j]+f(i,j+1,k-1,1,grid));
-            ans=min(ans,grid[i][j]+f(i-1,j,k,2,grid));
-            if(k>0) ans=min(ans,grid[i][j]+f(i+1,j,k-1,3,grid));
-        }
-        if(dir==4 || dir==3){
-            if(k>0) ans=min(ans,grid[i][j]+f(i,j-1,k-1,0,grid));
-            if(k>0) ans=min(ans,grid[i][j]+f(i,j+1,k-1,1,grid));
-            if(k>0) ans=min(ans,grid[i][j]+f(i-1,j,k-1,2,grid));
-            ans=min(ans,grid[i][j]+f(i+1,j,k,3,grid));
-        }
-        return dp[i][j][k][dir]=ans;
-    }
     int minCost(vector<vector<int>>& grid, int k) {
         int n=grid.size();
         int m=grid[0].size();
-        memset(dp,-1,sizeof(dp));
-        int ans=f(0,0,k,4,grid);
-        if(ans==1e9) return -1;
-        return ans;
+        priority_queue<vector<int>,vector<vector<int>>,greater<>> pq;
+        vector<vector<vector<int>>> vis(n,vector<vector<int>> (m,vector<int> (4,-1)));
+        for(int d=0;d<4;d++){
+            pq.push({grid[0][0],k,d,0,0});
+            vis[0][0][d]=k;
+        }
+        vector<int> dr1={0,0,-1,1};
+        vector<int> dr2={-1,1,0,0};
+        while(!pq.empty()){
+            auto curr=pq.top();
+            pq.pop();
+            int d=curr[0];
+            int val=curr[1];
+            int cnt=curr[2];
+            int i=curr[3];
+            int j=curr[4];
+            if(i==n-1 && j==m-1) return d;
+            for(int l=0;l<4;l++){
+                int ni=i+dr1[l];
+                int nj=j+dr2[l];
+                if(ni>=0 && nj>=0 && ni<n && nj<m){
+                    int nxtval;
+                    if(cnt==l) nxtval=val;
+                    else nxtval=val-1;
+                    if(nxtval>=0 && nxtval>vis[ni][nj][l]){
+                        vis[ni][nj][l]=nxtval;
+                        pq.push({d+grid[ni][nj],nxtval,l,ni,nj});
+                    }
+                }
+            }
+        }
+        return -1;
     }
 };
