8e92597802392dc6dfaaca66123cf87d6d6c7c62
mann220 <saraswatmanvendra@gmail.com>
2026-09-06 20:10:14 +0530

    Time: 642 ms (88.78%), Space: 257.8 MB (62.79%) - LeetHub

--- 1044-longest-duplicate-substring/1044-longest-duplicate-substring.cpp
+++ 1044-longest-duplicate-substring/1044-longest-duplicate-substring.cpp
@@ -1,17 +1,49 @@
 class Solution {
 public:
+    // String View method
+
+    // string f(int len,string &s){
+    //     int n=s.size();
+    //     int i=0;
+    //     int j=len;
+    //     unordered_map<string_view,int> mpp;
+    //     string_view p(s.data(),len);
+    //     while(j<=n){
+    //         mpp[p]++;
+    //         if(mpp[p]>=2) return string(p);
+    //         p=string_view(s.data()+i+1,len);
+    //         i++;
+    //         j++;
+    //     }
+    //     return "";
+    // }
+
+    // Rabin Karp
+    // best way to prevent leetcode antihash is unsigned long long
+    #define ull unsigned long long 
+
+    // ll mod=1e9+7;
+    ull radix=131;
+    ull hashValue(string &s,int len){
+        ull ans=0,factor=1;
+        for(int i=len-1;i>=0;i--){
+            ans+=((s[i]-'a')*factor);
+            factor=(factor*radix);
+        }
+        return ans;
+    }
     string f(int len,string &s){
         int n=s.size();
-        int i=0;
-        int j=len;
-        unordered_map<string_view,int> mpp;
-        string_view p(s.data(),len);
-        while(j<=n){
-            mpp[p]++;
-            if(mpp[p]>=2) return string(p);
-            p=string_view(s.data()+i+1,len);
-            i++;
-            j++;
+        ull max_weight=1;
+        unordered_map<ull,int> mpp;
+        for(int i=0;i<len;i++) max_weight=max_weight*radix;
+        ull hash1=hashValue(s,len);
+        ull hash2=0;
+        for(int i=0;i<=s.size()-len;i++){
+            if(i==0) hash2=hash1;
+            else hash2=((hash2*radix)-((s[i-1]-'a')*max_weight)+(s[i+len-1]-'a'));
+            mpp[hash2]++;
+            if(mpp[hash2]>=2) return s.substr(i,len);
         }
         return "";
     }
