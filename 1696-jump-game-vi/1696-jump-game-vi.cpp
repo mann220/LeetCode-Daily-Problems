d65358af691689d48349d0ea6ba69b4f599a54e7
mann220 <saraswatmanvendra@gmail.com>
2026-09-09 12:42:22 +0530

    Time: 20 ms (46.07%), Space: 85.2 MB (58.4%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
+    //Priority_queue solution of O(nlogn)
+    //     int n=nums.size();
+    //     priority_queue<pair<int,int>> pq;
+    //     vector<int> dp(n,0);
+    //     dp[n-1]=nums[n-1];
+    //     pq.push({dp[n-1],n-1});
+    //     for(int i=n-2;i>=0;i--){
+    //         while(!pq.empty() && pq.top().second>i+k) pq.pop();
+    //         auto [val,ind]=pq.top();
+    //         dp[i]=nums[i]+val;
+    //         pq.push({dp[i],i});
+    //     }
+    //     return dp[0];
+
+    // this priority queue solution has T.C=> O(nlogn) so we have to change it to O(n) by using Deque  
     int maxResult(vector<int>& nums, int k) {
+        // BY DEQUE IN O(N)
         int n=nums.size();
-        priority_queue<pair<int,int>> pq;
+        deque<int> dq;
+        dq.push_back(n-1);
         vector<int> dp(n,0);
         dp[n-1]=nums[n-1];
-        pq.push({dp[n-1],n-1});
         for(int i=n-2;i>=0;i--){
-            while(!pq.empty() && pq.top().second>i+k) pq.pop();
-            auto [val,ind]=pq.top();
-            dp[i]=nums[i]+val;
-            pq.push({dp[i],i});
+            while(!dq.empty() && dq.front()>i+k) dq.pop_front();
+            dp[i]=nums[i]+dp[dq.front()];
+            while(!dq.empty() && dp[dq.back()]<=dp[i]) dq.pop_back();
+            dq.push_back(i);
         }
         return dp[0];
     }
