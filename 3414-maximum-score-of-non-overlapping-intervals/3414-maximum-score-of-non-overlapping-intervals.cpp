81783e2174da459499ac008ba09d616b4ebc5656
mann220 <saraswatmanvendra@gmail.com>
2026-09-12 13:56:59 +0530

    Time: 2227 ms (20.65%), Space: 382.6 MB (20.65%) - LeetHub

---
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    #define ll long long
+    vector<int> next;
+    struct Node{
+        ll val=-1;
+        vector<int> ind;
+    };
+    vector<vector<Node>> dp;
+    int search(int i,vector<vector<int>> &intervals){
+        int lo=i+1;
+        int hi=intervals.size()-1;
+        int ans=intervals.size();
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(intervals[mid][0]>intervals[i][1]){
+                ans=mid;
+                hi=mid-1;
+            }
+            else lo=mid+1;
+        }
+        return ans;
+    }
+    Node f(int i,int cnt,vector<vector<int>> &intervals){
+        if(i==intervals.size() || cnt==0) return Node();
+        if(dp[i][cnt].val!=-1) return dp[i][cnt];
+        Node nttake=f(i+1,cnt,intervals);
+        int nextIndex=next[i];
+        Node temp=f(nextIndex,cnt-1,intervals);
+        Node take;
+        take.val=1LL*intervals[i][2]+temp.val;
+        take.ind=temp.ind;
+        take.ind.push_back(intervals[i][3]);
+        sort(take.ind.begin(),take.ind.end());
+        Node result;
+        if(nttake.val>take.val) result=nttake;
+        else if(nttake.val<take.val) result=take;
+        else result=(take.ind<nttake.ind) ? take:nttake;
+        return dp[i][cnt]=result;
+    }
+    vector<int> maximumWeight(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        for(int i=0;i<n;i++) intervals[i].push_back(i);
+        sort(intervals.begin(),intervals.end());
+        dp=vector<vector<Node>> (n,vector<Node> (5));
+        next=vector<int> (n);
+        for(int i=0;i<n;i++) next[i]=search(i,intervals);
+        return f(0,4,intervals).ind;
+    }
+};
