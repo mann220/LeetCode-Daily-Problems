8e8cee15c4ec1c43d3de3a7c9a909d85feb0f90d
mann220 <saraswatmanvendra@gmail.com>
2026-09-12 14:01:22 +0530

    Time: 2300 ms (19.56%), Space: 381 MB (21.74%) - LeetHub

--- 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
@@ -1,78 +1,52 @@
 class Solution {
 public:
-    struct Node {
-        long long val = -1;
-        vector<int> ind; // Changed to vector<int> to match the required return type
+    #define ll long long
+    struct Node{
+        ll val=-1;
+        vector<int> ind;
     };
-
+    vector<vector<Node>> dp;
+    int search(int i,vector<vector<int>> &intervals){
+        int lo=i+1;
+        int hi=intervals.size()-1;
+        int ans=intervals.size();
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(intervals[mid][0]>intervals[i][1]){
+                ans=mid;
+                hi=mid-1;
+            }
+            else lo=mid+1;
+        }
+        return ans;
+    }
+    Node f(int i,int cnt,vector<vector<int>> &intervals){
+        if(i==intervals.size() || cnt==0) return Node();
+        if(dp[i][cnt].val!=-1) return dp[i][cnt];
+        Node nttake=f(i+1,cnt,intervals);
+        int nextIndex=search(i,intervals);
+        Node temp=f(nextIndex,cnt-1,intervals);
+        Node take;
+        take.val=1LL*intervals[i][2]+temp.val;
+        take.ind=temp.ind;
+        take.ind.push_back(intervals[i][3]);
+        sort(take.ind.begin(),take.ind.end());
+        Node result;
+        if(nttake.val>take.val) result=nttake;
+        else if(nttake.val<take.val) result=take;
+        else result=(take.ind<nttake.ind) ? take:nttake;
+        return dp[i][cnt]=result;
+    }
     vector<int> maximumWeight(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        // Add original indices
-        for(int i = 0; i < n; i++) {
-            intervals[i].push_back(i);
-        }
-        
-        // Sort by start times
-        sort(intervals.begin(), intervals.end());
-        
-        // Precalculate the next valid non-overlapping interval for every interval
-        vector<int> next_idx(n, n);
-        for(int i = 0; i < n; i++) {
-            int lo = i + 1;
-            int hi = n - 1;
-            int ans = n;
-            while(lo <= hi) {
-                int mid = lo + (hi - lo) / 2;
-                
-                if(intervals[mid][0] > intervals[i][1]) {
-                    ans = mid;
-                    hi = mid - 1;
-                } else {
-                    lo = mid + 1;
-                }
-            }
-            next_idx[i] = ans;
-        }
-        
-        // DP Table: initialized to size (n+1) x 5
-        vector<vector<Node>> dp(n + 1, vector<Node>(5));
-        
-        // Base cases (val = 0 for empty states)
-        for(int i = 0; i <= n; i++) {
-            for(int cnt = 0; cnt <= 4; cnt++) {
-                dp[i][cnt].val = 0;
+        int n=intervals.size();
+        for(int i=0;i<n;i++) intervals[i].push_back(i);
+        sort(intervals.begin(),intervals.end());
+        dp=vector<vector<Node>> (n,vector<Node> (5));
+        for(int i=n-1;i>=0;i--){
+            for(int j=1;j<=4;j++){
+
             }
         }
-        
-        // Fill DP table backwards
-        for(int i = n - 1; i >= 0; i--) {
-            for(int cnt = 1; cnt <= 4; cnt++) {
-                
-                // Option 1: Do not take the current interval
-                Node nttake = dp[i+1][cnt];
-                
-                // Option 2: Take the current interval
-                int nxt = next_idx[i];
-                Node temp = dp[nxt][cnt-1];
-                
-                Node take;
-                take.val = (long long)intervals[i][2] + temp.val;
-                take.ind = temp.ind;
-                take.ind.push_back(intervals[i][3]);
-                sort(take.ind.begin(), take.ind.end());
-                
-                // Decide which option is better
-                if(nttake.val > take.val) {
-                    dp[i][cnt] = nttake;
-                } else if(nttake.val < take.val) {
-                    dp[i][cnt] = take;
-                } else {
-                    // Tie-breaker: Lexicographically smaller indices
-                    dp[i][cnt] = (take.ind < nttake.ind) ? take : nttake;
-                }
-            }
-        }
-        
-        return dp[0][4].ind;
+        return f(0,4,intervals).ind;
     }
 };
