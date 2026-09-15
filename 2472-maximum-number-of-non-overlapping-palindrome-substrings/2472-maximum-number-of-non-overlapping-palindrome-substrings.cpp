34299950b20349ea6287b0122478f4306873d510
mann220 <saraswatmanvendra@gmail.com>
2026-09-15 11:40:27 +0530

    Time: 1904 ms (5.23%), Space: 209.7 MB (14.92%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
+    bool isPalindrome(const string& s, int i, int j) {
+        while (i < j) {
+            if (s[i++] != s[j--]) return false;
+        }
+        return true;
+    }
+
+    //max non-overlapping palindromic pieces (length >= k) from s[i..j]
+    int solve(const string& s, int k, int i, int j, vector<vector<int>>& t) {
+        int n = s.size();
+        if (i >= n || j >= n) 
+            return 0;
+            
+        if (t[i][j] != -1) 
+            return t[i][j];
+
+        if (isPalindrome(s, i, j)) {
+            int growWindow  = solve(s, k, i, j + 1, t);
+            int takeIt      = 1 + solve(s, k, j + 1, j + k, t);
+            int slideWindow = solve(s, k, i + 1, j + 1, t);
+
+            return t[i][j] = max({growWindow, takeIt, slideWindow});
+        }
+
+        int slideWindow = solve(s, k, i + 1, j + 1, t);
+        int growWindow  = solve(s, k, i, j + 1, t);
+
+        return t[i][j] = max(slideWindow, growWindow);
+    }
+
     int maxPalindromes(string s, int k) {
-        int n=s.length();
-        vector<vector<bool>> isPalindrome(n+1, vector<bool>(n+1, false));
-        for(int L=1;L<=n;L++){
-            for(int i=0;i+L<=n;i++){
-                int j=i+L-1;
-                if(i==j) isPalindrome[i][j]=true; // length 1
-                else if(i+1==j) isPalindrome[i][j]=(s[i]==s[j]);
-                else isPalindrome[i][j]=(s[i]==s[j] && isPalindrome[i+1][j-1]);
-            }
-        }
-        vector<int> dp(n+1, 0);
-        for(int L=k;L<=n;L++){
-            int result=dp[L-1];
-            int j=L-1;
-            for(int i=0;j-i+1>=k;i++){
-                if(isPalindrome[i][j]){
-                    result=max(result,1+dp[i]);
-                }
-            }
-            dp[L]=result;
-        }
-        return dp[n];
+        int n = s.size();
+        if (k == 1)
+            return n; //each character can be a substring
+
+        vector<vector<int>> t(n, vector<int>(n, -1));
+
+        return solve(s, k, 0, k - 1, t);
     }
 };
