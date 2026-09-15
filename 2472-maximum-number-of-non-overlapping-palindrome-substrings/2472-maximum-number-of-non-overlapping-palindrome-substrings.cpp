c20d12435d43eceebd6fb52d6b93ba189a7bb3b2
mann220 <saraswatmanvendra@gmail.com>
2026-09-15 11:40:57 +0530

    Time: 861 ms (10.21%), Space: 217.8 MB (13.87%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,11 +1,9 @@
+//Approach-2 (Using Blueprint of Palindrome Strings to conver isPalindrome check to O(1))
+//T.C : O(n^2)
+//S.C : O(n^3)
 class Solution {
 public:
-    bool isPalindrome(const string& s, int i, int j) {
-        while (i < j) {
-            if (s[i++] != s[j--]) return false;
-        }
-        return true;
-    }
+    vector<vector<bool>> isPalindrome;
 
     //max non-overlapping palindromic pieces (length >= k) from s[i..j]
     int solve(const string& s, int k, int i, int j, vector<vector<int>>& t) {
@@ -16,7 +14,7 @@ public:
         if (t[i][j] != -1) 
             return t[i][j];
 
-        if (isPalindrome(s, i, j)) {
+        if (isPalindrome[i][j]) { //O(1)
             int growWindow  = solve(s, k, i, j + 1, t);
             int takeIt      = 1 + solve(s, k, j + 1, j + k, t);
             int slideWindow = solve(s, k, i + 1, j + 1, t);
@@ -34,6 +32,22 @@ public:
         int n = s.size();
         if (k == 1)
             return n; //each character can be a substring
+
+        isPalindrome.assign(n+1, vector<bool>(n+1));
+        //Palindromic substring Blueprint
+        for(int L = 1; L <= n; L++) {
+            for(int i = 0; i+L <= n; i++) {
+                int j = i + L - 1;
+
+                if(i == j) {
+                    isPalindrome[i][i] = true; //Single characters are palindrome
+                } else if(i+1 == j) {
+                    isPalindrome[i][j] = (s[i] == s[j]); //Strings of 2 Length
+                } else {
+                    isPalindrome[i][j] = ((s[i] == s[j]) && isPalindrome[i+1][j-1] == true);
+                }
+            }
+        }
 
         vector<vector<int>> t(n, vector<int>(n, -1));
 
