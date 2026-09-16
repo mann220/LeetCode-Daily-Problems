70eb6e3493440f2f8dad623f6acf39e4e57ea618
mann220 <saraswatmanvendra@gmail.com>
2026-09-16 15:04:18 +0530

    Time: 83 ms (30.37%), Space: 95.4 MB (23.36%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -2,20 +2,30 @@ class Solution {
 public:
     int mod=1e9+7;
     int sz;
-    vector<vector<int>> dp;
-    int f(int n,int k){
-        if(k>n || k<0) return 0;
-        if(n==0){
-            if(k==0) return 1;
-            return 0;
-        }
-        if(dp[n][k]!=-1) return dp[n][k];
-        int opt1=f(n-1,k); // we skipped that length
-        int opt2=f(n-1,k-1); // we draw a line on that length
-        return dp[n][k]=(opt1+opt2)%mod;
-    }
+    // we are taking here points in which it is open means line start and close
+    // vector<vector<int>> dp;
+    // int f(int n,int k,int i){
+    //     if(i>=n) return 0;
+    //     if(k==0) return 1;
+    //     int take=0;
+    //     for(int j=i+1;j<n;j++) take=(take+f(n,k-1,j))%mod;
+    //     int skip=f(n,k,i+1)%mod;
+    //     return take+skip;
+    // }
+    #define ll long long
     int numberOfSets(int n, int k) {
-        dp=vector<vector<int>> (n+k+1,vector<int> (2*k+1,-1));
-        return f(n+k-1,2*k)%mod;
+        vector<vector<ll>> dp(n+1,vector<ll>(k+1,0));
+        for(int i=0;i<n;i++) dp[i][0]=1;
+        for(int j=1;j<=k;j++){
+            vector<ll> pre(n+1,0);
+            for(int x=n-1;x>=0;x--) pre[x]=(pre[x+1]+dp[x][j-1])%mod;
+            for(int i=n-1;i>=0;i--){
+                ll nttk=dp[i+1][j];
+                ll tk=0;   
+                tk=(tk+pre[i+1])%mod;
+                dp[i][j]=(tk+nttk)%mod;
+            }
+        }
+        return dp[0][k];
     }
 };
