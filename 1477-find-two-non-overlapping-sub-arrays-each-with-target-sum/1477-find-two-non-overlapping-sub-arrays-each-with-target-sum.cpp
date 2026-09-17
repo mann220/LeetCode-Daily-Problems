fd2b98f82f318c5994f24505115399993657b6d2
mann220 <saraswatmanvendra@gmail.com>
2026-09-17 11:40:11 +0530

    Time: 3 ms (94.59%), Space: 88.8 MB (79.38%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     int minSumOfLengths(vector<int>& arr, int target) {
         int n=arr.size();
-        unordered_map<int,int> mpp;
+        int i=0;
+        int j=0;
         int pre=0;
-        mpp[0]=-1;
         vector<int> minIndex(n,INT_MAX);
         int ans=INT_MAX;
         int bestMin=INT_MAX;
-        for(int i=0;i<n;i++){
-            pre+=arr[i];
-            int rem=pre-target;
-            if(mpp.find(rem)!=mpp.end()){
-                int len=i-mpp[rem];
-                if(mpp[rem]>=0 && minIndex[mpp[rem]]!=INT_MAX){
-                    ans=min(ans,len+minIndex[mpp[rem]]);
-                }
+        while(j<n){
+            pre+=arr[j];
+            while(pre>target){
+                pre-=arr[i];
+                i++;
+            }
+            if(pre==target){
+                int len=j-i+1;
+                if(i>0 && minIndex[i-1]!=INT_MAX) ans=min(ans,len+minIndex[i-1]);
                 bestMin=min(bestMin,len);
             }
-            minIndex[i]=bestMin;
-            mpp[pre]=i;
+            minIndex[j]=bestMin;
+            j++;
         }
         return ans==INT_MAX ? -1:ans;
     }
